0e9b5a05d0304e733142515d34930259bc4c18e4
gdmec07150637 <2767143496@qq.com>
2016-12-25 16:30:08 +0800

    APK

--- app/src/main/java/com/example/s07150632/menu/MainActivity.java
+++ app/src/main/java/com/example/s07150632/menu/MainActivity.java
@@ -1,4 +1,4 @@
-package cn.edu.gdmec.s07150637.menu;
+package  com.example.s07150632.menu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,40 +10,48 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 public class MainActivity extends AppCompatActivity {
-private TextView tv1;
+    private TextView tv1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv1= (TextView) findViewById(R.id.textView);
+        tv1 = (TextView)findViewById(R.id.textView);
+        registerForContextMenu(tv1);
     }
-//创建选项菜单
+
+    //创建选项菜单
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-      //  MenuInflater mi  =getMenuInflater();
-       // mi.inflate(R.menu.menu,menu);
-       menu.add(0,1,0,R.string.m1);
-       menu.add(0,1,0,R.string.m2);
+        //加载menu.xml
+        MenuInflater mi = getMenuInflater();
+        mi.inflate(R.menu.menu,menu);
+        menu.add(0,1,0,R.string.m1);
+        menu.add(0,1,0,R.string.m2);
         return super.onCreateOptionsMenu(menu);
     }
-//选项菜单被选中
+    //选项菜单被选中
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,item.getTitle(),Toast.LENGTH_LONG).show();
         return super.onOptionsItemSelected(item);
     }
-//创建上下文菜单
+    //创建上下文菜单
+
+
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
         menu.add(0,1,0,R.string.m1);
         menu.add(0,1,0,R.string.m2);
+
     }
-//上下文菜单被选中
+    //上下文菜单被选中
+
     @Override
     public boolean onContextItemSelected(MenuItem item) {
-        Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,item.getTitle(),Toast.LENGTH_LONG).show();
         return super.onContextItemSelected(item);
     }
 }
